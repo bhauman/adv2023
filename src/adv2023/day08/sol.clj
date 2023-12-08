991edbd3b3d67d6475b63a9a444b264842c98b8b
Bruce Hauman <bhauman@gmail.com>
2023-12-08 14:37:40 -0600

    day 8

---
+++ src/adv2023/day08/sol.clj
@@ -0,0 +1,75 @@
+(ns adv2023.day8.sol
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as string]
+   [adv2023.grid :as grid]
+   [clojure.set :as set]
+   [clojure.edn :as edn]))
+
+(defn parse [[a b & rs]]
+  {:directions a
+   :locations (as-> rs x
+              (string/join x)
+              (string/replace x "=" "")
+              (str "{" x "}")
+              (edn/read-string x))})
+
+(def input
+  (->> (slurp "src/adv2023/day08/input.txt")
+       (string/split-lines)
+       parse))
+
+(defn location-seq [start {:keys [directions locations]}]
+  (reductions (fn [loc dir]
+                ((if (= dir \L) first second)
+                 (get locations loc)))
+              start
+              (cycle directions)))
+
+;; part 1
+#_(->> (location-seq 'AAA input)
+       (take-while #(not= 'ZZZ %))
+       count) ;; => 16531
+
+;; doesn't work past a certain point but its a quick impl
+(defn seive
+  ([] (seive 2 (range 2 5000)))
+  ([cur-prime next-nums]
+   (cons cur-prime
+         (lazy-seq
+          (let [[next-prime & rest-nums] (remove #(zero? (mod % cur-prime)) next-nums)]
+            (seive next-prime rest-nums))))))
+
+(defn is-prime? [x]
+  (->> (seive)
+       (take-while #(<= % x))
+       (filter #(= x %))
+       first))
+
+(defn part2 [{:keys [directions locations]}]
+  (let [dir-length (count directions)
+        start-nodes (->> (keys locations)
+                         (filter  #(= \A (last (str  %)))))
+        cycle-lengths (map (fn [node]
+                             (->> input
+                                  (location-seq node)
+                                  (take-while #(not= \Z (last (str %))))
+                                  count))
+                           start-nodes)
+        factors (map #(/ % dir-length) cycle-lengths)]
+    (assert (is-prime? dir-length))
+    (assert (every? is-prime? factors))
+    (assert (apply not= factors))
+    (apply * dir-length factors)))
+
+;;(part2 input)
+;; => 24035773251517
+
+
+
+
+
+
+
+
+
